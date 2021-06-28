d15ed918f76d86dd8d7a06b44f5773a1e056feb2
HassanKhan123 <hk2602809@gmail.com>
2021-06-28 12:31:40 +0500

    benchmarking refactoring

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
 
 if (cluster.isMaster) {
   cluster.fork();
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
 } else {
+  const crypto = require('crypto');
   const express = require('express');
   const app = express();
 
-  const doWork = duration => {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  };
-
   app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hi there');
+    });
   });
 
   app.get('/fast', (req, res) => {
-    // doWork(5000);
     res.send('This is fast');
   });
 
